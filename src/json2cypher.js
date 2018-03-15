6af63e62d84639ab4d8df30be18a4807a798b867
Moxious <moxious@oldhat.org>
2018-03-15 12:50:01 -0400

    command line option for multi-line reading

--- src/json2cypher.js
+++ src/json2cypher.js
@@ -2,29 +2,81 @@ import json2neo from './index';
 import yargs from 'yargs';
 import fs from 'fs';
 import JSONMapping from './mapping/JSONMapping';
+import getStdin from 'get-stdin';
+import readline from 'readline';
 
-if (!yargs.argv.file || !yargs.argv.label) {
-  throw new Error('Call me with --file /path/to/json --label NodeLabel');
-}
+const help = () => {
+  console.log(`
+json2cypher [args]
+   --stdin (read JSON data from standard in)
+   --file /path/to/file.json (read JSON data from specified file)
+   --mapping /path/to/mapping-file.json
+   --label MyLabel (Use this label for the root JSON object)
+   --multi treat input source as multiple JSON objects, one per line.
+Produces cypher on standard output.
+`);
+  process.exit(1);
+};
 
-const data = fs.readFileSync(yargs.argv.file);
-const obj = JSON.parse(data);
+const initMapping = () => {
+  let mappingData = {};
 
-let mappingData = {};
-
-if (yargs.argv.mapping) {
-  try {
-    const data = fs.readFileSync(yargs.argv.mapping);
-    mappingData = JSON.parse(data);
-  } catch(e) {
-    console.error('Bad mapping data or file provided');
-    console.error(e);
-    process.exit(1);
+  if (yargs.argv.mapping) {
+    try {
+      const data = fs.readFileSync(yargs.argv.mapping);
+      mappingData = JSON.parse(data);
+    } catch (e) {
+      console.error('Bad mapping data or file provided');
+      console.error(e);
+      process.exit(1);
+    }
   }
-}
 
-let mapping = new JSONMapping(mappingData);
-const cache = json2neo(obj, yargs.argv.label, mapping);
-console.log(cache.cypher());
+  return new JSONMapping(mappingData);
+};
+
+const main = () => {
+  if (yargs.argv.help) {
+    help();
+  }
+
+  const mapping = initMapping();
+
+  const onRawJSONObject = obj => {
+    const cache = json2neo(obj, yargs.argv.label, mapping);
+    console.log(cache.cypher());
+  };
+
+  if (yargs.argv.stdin) {
+    if (yargs.argv.multi) {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false,
+      });
+
+      rl.on('line', line => line ? onRawJSONObject(JSON.parse(line)) : null);
+    } else {
+      getStdin()
+        .then(rawData => onRawJSONObject(JSON.parse(rawData)))
+        .catch(err => console.error('Failed to parse input data', err));
+    }
+  } else {
+    if (yargs.argv.multi) {
+      const rl = readline.createInterface({
+        input: fs.createReadStream(yargs.argv.file),
+        output: process.stdout,
+        terminal: false,
+      });
+
+      rl.on('line', line => line ? onRawJSONObject(JSON.parse(line)) : null);
+    } else {
+      const data = fs.readFileSync(yargs.argv.file);
+      onRawJSONObject(JSON.parse(data));
+    }
+  }
+};
+
+main();
 
 
